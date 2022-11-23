18ce72539e659a534c5c2fe12257ff2210d516c7
JoseRamonHernandez <joseramonhernandezmunoz20@gmail.com>
2022-11-23 12:43:32 -0600

    borrar suscripciones que ya no son validas

--- server/push.js
+++ server/push.js
@@ -2,7 +2,7 @@ const urlsafeBase64 = require('urlsafe-base64');
 const vapid = require('./vapid.json');
 const fs = require('fs');
 
-const suscripciones = require('./subs-db.json');
+let  suscripciones = require('./subs-db.json');
 
 const webpush = require('web-push');
 
@@ -25,15 +25,33 @@ module.exports.addSubscription = ( suscripcion ) => {
 };
 
 module.exports.sendPush = ( post ) => {
+    console.log('Mandando PUSHES');
 
+    let notificacionesEnviadas = [];
 
     suscripciones.forEach( (suscripcion, i) => {
 
 
-        webpush.sendNotification( suscripcion, JSON.stringify( post )  );
+        const pushProm = webpush.sendNotification( suscripcion, JSON.stringify( post ) )
+        .then( console.log('Notificación enviada') )
+        .catch( err => {
 
+            console.log('Notificación falló');
+
+            if ( err.statusCode === 410 ) {
+                suscripciones[i].borrar = true;
+            }
+        });
+
+        notificacionesEnviadas.push( pushProm );
 
     });
 
+    Promise.all( notificacionesEnviadas ).then( () => {
+
+        suscripciones = suscripciones.filter( subs => !subs.borrar );
+
+        fs.writeFileSync(`${ __dirname }/subs-db.json`, JSON.stringify(suscripciones) );
+    });
 
 };
